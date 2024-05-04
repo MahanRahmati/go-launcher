26fa1530262bb5a0964f7ad1c09dc5f98fe5b6d3
Mahan <mahan.rahmati@protonmail.com>
2024-05-04 18:13:19 -0400

    feat: get arguments

--- input.go
+++ input.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func getUserInput() string {
+	arg := os.Args[1]
+	if arg != "" {
+		return arg
+	}
 	var input string
 	fmt.Scan(&input)
 	return input
